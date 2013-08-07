35b782446395ea340eb7aedb31e0a11b41280912
alexthompson288 <alexthompson288@gmail.com>
2013-08-07 17:55:43 +0100

    created new page for measuring scores using chart.js

---
+++ app/static/challenge.js
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Styles -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="css/bootstrap-overrides.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/theme.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+
+    <link rel="stylesheet" type="text/css" href="css/lib/animate.css" media="screen, projection">
+    <link rel="stylesheet" href="css/_fpanels.css" type="text/css" media="screen" />
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="navbar navbar-static-top">
+          <div class="navbar-inner">
+            <div class="container">
+                <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </a>
+                <h1 id="logo-symbol">
+                    <a class="brand logo" id="logo-symbol-link" href="index.html">
+                        
+                    </a>
+                </h1>
+                <div class="nav-collapse collapse">
+                    <ul class="nav pull-right">
+						<li><a href="_features.html">FEATURES</a></li>
+                        <li><a href="_download.html">DOWNLOAD</a></li>
+                        <li><a href="_about-us.html">ABOUT</a></li>
+                        <li><a href="_education-for-parents.html">FOR PARENTS</a></li>
+                        <li><a href="_login.html">LOGIN</a></li>
+                    </ul>
+                </div>
+            </div>
+          </div>
+        </div>
+    <div class="navbar navbar-inverse">
+      <div class="navbar-inner">
+        <div class="container">
+
+        </div>
+      </div>
+    </div>
+
+    <div id="fpanels">
+        <div class="not-ready-yet">
+            
+        </div>
+
+    </div>
+
+    <!-- starts footer -->
+    <footer id="footer">
+        <div class="yellow-background">
+            <div class="container">
+                <div class="row social">
+                    <div class="row info">
+                        <div class="span12">
+                            <div class="span12">
+                                <h4>You can follow us on: </h4>
+                                <a href="http://www.facebook.com/learntoreadwithpip" class="facebook">
+                                    
+                                    <span class="socialicons_h ico1h"></span>
+                                </a>
+                                <a href="http://www.twitter.com/learntoreadwithpip" class="twitter">
+                                    
+                                    <span class="socialicons_h ico2h"></span>
+                                </a>
+                               
+                                <a href="http://www.pinterest.com/learntoreadwithpip" class="pinterest">
+                                   
+                                    <span class="socialicons_h ico5h"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="magicbook-background padding-top-50">
+            <div class="container">
+                <div class="row info">
+                    <div class="span2">
+                        <h5><a href="index.html">Home</a></h5>
+                    </div>
+                   
+                   
+                    <div class="span2">
+                        <h5><a href="_features.html">Features</a></h5>
+                        <p><a href="_features.html#overview">Overview</a></p>
+                        <p><a href="_features.html#curriculum">Curriculum</a></p>
+                        <p><a href="_features.html#magicbook">Magic Book</a></p>
+                        <p><a href="_features.html#games">Games</a></p>
+                    </div>
+
+                     <div class="span2">
+                        <h5><a href="_download.html">Download</a></h5>
+                    </div>
+                    <div class="span2">
+                        <h5><a href="_about-us.html">About</a></h5>
+                        <p><a href="_about-us.html#team">Team</a></p>
+                        <p><a href="_about-us.html#contact">Contact us</a></p>
+                    </div>
+                    
+                    <div class="span2">
+                        <h5>Resources</h5>
+                        <p><a href="_characters.html">Characters</a></p>
+                        <p><a href="_story.html">The Story</a></p>
+                        <p><a href="assets/terms.pdf" target="_blanK">Terms</a></p>
+                        <p><a href="assets/privacy.pdf" target="_blank">Privacy</a></p>
+
+                    </div>
+                    
+                    
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row info">
+                    <div class="span12">
+                        
+                        <div class="row copyright text-center">
+                            <div class="span12">
+                                © 2013 Altitude Education Ltd.
+                            </div>
+                        </div>
+                    </div>            
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+    <script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-42172602-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+    </script>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.countdown.min.js"></script>
+    <script src="js/theme.js"></script>
+
+</body>
+</html>
